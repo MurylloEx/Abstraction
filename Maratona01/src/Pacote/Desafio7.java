ddf750cc0c58748a59239117d8c7ce6e85855f3c
Muryllo Pimenta <32225687+MurylloEx@users.noreply.github.com>
2019-09-25 09:14:07 -0300

    Update Desafio7.java

--- Maratona01/src/Pacote/Desafio7.java
+++ Maratona01/src/Pacote/Desafio7.java
@@ -1,63 +1,73 @@
 package Pacote;
 
 import java.util.Scanner;
+import java.util.Random;
 
 public class Desafio7 {
 	
 	public static void main(String[] args) {
-		int count = 0;
-		int votosChapa1 = 0;
-		int votosChapa2 = 0;
-		int votosBrancos = 0;
-		int votosNulos = 0;
-		Scanner sc = new Scanner(System.in);
-		
-		while (count < 5) {
-			System.out.println("Digite um número para escolher a chapa.");
-			try {
-				int numero = Integer.parseInt(sc.nextLine());
-				switch(numero) {
-					case 0:
-						votosBrancos += 1;
-						break;
-					case 1:
-						votosChapa1 += 1;
-						break;
-					case 2:
-						votosChapa2 += 1;
-						break;
-					default:
-						votosNulos += 1;
-						break;
+		try {
+			int count = 0;
+			int votosChapa1 = 0;
+			int votosChapa2 = 0;
+			int votosBrancos = 0;
+			int votosNulos = 0;
+			Scanner sc = new Scanner(System.in);
+			
+			Random rnd = new Random();
+			
+			while (count < 5) {
+				System.out.println("Computando votos.");
+				try {
+					int numero = rnd.nextInt(4);
+					switch(numero) {
+						case 0:
+							votosBrancos += 1;
+							break;
+						case 1:
+							votosChapa1 += 1;
+							break;
+						case 2:
+							votosChapa2 += 1;
+							break;
+						default:
+							votosNulos += 1;
+							break;
+					}
 				}
+				catch(Exception ex) {
+					System.out.println("Digite um nÃºmero vÃ¡lido.");
+					count -=1;
+				}
+				count += 1;
+				sc = new Scanner(System.in);
+				Thread.sleep(1000);
 			}
-			catch(Exception ex) {
-				System.out.println("Digite um número válido.");
-				count -=1;
-			}
-			count += 1;
-			sc = new Scanner(System.in);
-		}
-		
-		sc.close();
-		
-		System.out.println("Número de votos da para a chapa 1: " + votosChapa1);
-		System.out.println("Número de votos da para a chapa 2: " + votosChapa2);
-		System.out.println("Número de votos em branco: " + votosBrancos);
-		System.out.println("Número de votos nulos: " + votosNulos);
-		if (votosChapa1 > votosChapa2) {
-			//Chapa 1 ganhou.
-			System.out.println("[-->] Chapa 1 ganhou.");
-		}
-		else {
-			if (votosChapa1 == votosChapa2) {
-				//Deu empate.
-				System.out.println("[-->] Deu empate.");
+			
+			sc.close();
+			
+			System.out.println("NÃºmero de votos da para a chapa 1: " + votosChapa1);
+			System.out.println("NÃºmero de votos da para a chapa 2: " + votosChapa2);
+			System.out.println("NÃºmero de votos em branco: " + votosBrancos);
+			System.out.println("NÃºmero de votos nulos: " + votosNulos);
+			if (votosChapa1 > votosChapa2) {
+				//Chapa 1 ganhou.
+				System.out.println("[-->] Chapa 1 ganhou.");
 			}
 			else {
-				//Chapa 2 ganhou.
-				System.out.println("[-->] Chapa 2 ganhou.");
+				if (votosChapa1 == votosChapa2) {
+					//Deu empate.
+					System.out.println("[-->] Deu empate.");
+				}
+				else {
+					//Chapa 2 ganhou.
+					System.out.println("[-->] Chapa 2 ganhou.");
+				}
 			}
+			
+		}
+		catch(Exception e) {
+			
 		}
 		
 	}
